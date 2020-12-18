2ae1d3dc58faaccd27322532a53ff6f64a26e775
Mirko Nasato <mirko@encoded.io>
2020-12-18 08:57:49 +0000

    Simplify project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,16 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Router Sample</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        Add content here.
       </IonContent>
     </IonPage>
   );
